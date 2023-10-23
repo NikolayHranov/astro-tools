9a31a0e356398d6577c3018f39396067ee0dd4ea
NikolayHranov <nikolay.hranov@gmail.com>
2023-10-23 23:32:21 +0300

    Commit main win 2.0

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import ttk
 
 
-APP_NAME = "AnlysIMG"
+APP_NAME = "Astro Tools"
 
 
 class Styles(ttk.Style):
@@ -18,14 +18,23 @@ class MainWin(tk.Tk):
         self.title(APP_NAME)
 
         self.appinfoFrame = tk.Frame(self)
-        self.appinfoFrame.pack(anchors="nw", padx=10, pady=10)
+        self.appinfoFrame.pack(padx=10, pady=10, anchor="nw")
         self.appinfoFrame.title = ttk.Label(self.appinfoFrame, text=APP_NAME, style="Title.TLabel")
         self.appinfoFrame.title.pack()
 
         self.navigationFrame = tk.Frame(self)
-        self.navigationFrame.pack(anchor="ne", padx=10, pady=10)
-        self.navigationFrame.button = ttk.Button(self.navigationFrame, text="Button")
-        self.navigationFrame.button.pack()
+        self.navigationFrame.pack(padx=20, pady=20, anchor="ne")
+        self.navigationFrame.button_stack = ttk.Button(self.navigationFrame, text="Stack images", width=20, command=lambda:self.navigate("stacking"))
+        self.navigationFrame.button_stack.pack()
+    def navigate(self, location):
+        match location:
+            case "stacking":
+                pass
+
+
+class StackWin(tk.Toplevel):
+    def __init__(self):
+        super().__init__()
 
 
 if __name__ == "__main__":
