6034f6488996c45053e345e11cda5cb0de94fb50
pesh007 <petarmpopov07@gmail.com>
2023-10-26 21:34:24 +0300

    dobavka za imenata

--- sybirane_na_directories.py
+++ sybirane_na_directories.py
@@ -20,27 +20,32 @@ selected_end_directory = None
 def select_light_directory():
     global selected_light_directory
     selected_light_directory = filedialog.askdirectory(title="select light directory")
-    printtk(selected_light_directory)
+    light = "light: "+selected_light_directory
+    printtk(light)
 
 def select_dark_directory():
     global selected_dark_directory
     selected_dark_directory = filedialog.askdirectory(title="select dark directory")
-    printtk(selected_dark_directory)
+    dark = "dark: "+selected_dark_directory
+    printtk(dark)
 
 def select_darkflat_directory():
     global selected_darkflat_directory
     selected_darkflat_directory = filedialog.askdirectory(title="select darkflat directory")
-    printtk(selected_darkflat_directory)
+    darkflat = "darkflat: "+selected_darkflat_directory
+    printtk(darkflat)
 
 def select_flat_directory():
     global selected_flat_directory
     selected_flat_directory = filedialog.askdirectory(title="select flat directory")
-    printtk(selected_flat_directory)
+    flat = "flat: "+selected_flat_directory
+    printtk(flat)
 
 def select_end_directory():
     global selected_end_directory
     selected_end_directory = filedialog.askdirectory(title="select end directory")
-    printtk(selected_end_directory)
+    end = "end: "+selected_end_directory
+    printtk(end)
 
 button = tk.Button(root, text="Select light directory", command=select_light_directory)
 button.pack()
