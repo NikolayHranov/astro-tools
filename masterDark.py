d5b61347ca7f8b69ab357a1cf0022d965a3b7a25
pesh007 <petarmpopov07@gmail.com>
2024-04-09 23:15:44 +0300

    substract

--- masterDark.py
+++ masterDark.py
@@ -1,9 +1,17 @@
-from astropy import fits
+from astropy.io import fits
 import numpy as np 
 import statistics as st
 import os
+from os import walk
+
 def creat_master_dark(darks_directory):
-    darks = os.dirlist(darks_directory)
+    darks1 = os.listdir(darks_directory)
+
+    darks = []
+
+    for dark in darks1:
+        directory = "/home/petar-popov/Desktop/darks_venus/" + dark
+        darks.append(directory)
     fitsFile=darks[0]
     light = fits.open(fitsFile)
     data1 = light[0].data
@@ -11,14 +19,14 @@ def creat_master_dark(darks_directory):
     dimensions = data.shape
     dimensions3D = (int(len(darks)),) + dimensions
     masterDark3D = np.zeros(dimensions3D)
-    z = 0
+
     for dark in darks:
+        z = darks.index(dark)
         dark = fits.open(dark)
         dark = np.array(dark[0].data)
         for x in range(dimensions3D[2]):
             for y in range(dimensions3D[1]):
                 masterDark3D[z, y, x] = dark[y, x]
-                z += 1
     
     masterDark = np.zeros(dimensions)
     for x in range(dimensions[1]):
@@ -29,3 +37,7 @@ def creat_master_dark(darks_directory):
                 masterDark[y, x] = st.median(values)
 
     return masterDark
+
+mdark = creat_master_dark(darks_directory="/home/petar-popov/Desktop/darks_venus")
+#darks = os.listdir("/home/petar-popov/Desktop/darks_venus")
+#print(darks)
